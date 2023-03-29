f4aba69589c87a8a7dac21e9a7d734951776fbce
Paul Sturtz <sturtzpaul@gmail.com>
2023-03-29 15:12:49 -0400

    API

--- server/controllers/lyricsapiController.js
+++ server/controllers/lyricsapiController.js
@@ -1,47 +1,75 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const apiKey = process.env.LYRICS_API_KEY; // defined in .env
-const baseUrl = 'https://api.musixmatch.com/ws/1.1/'; // potentially just have one url variable which is baseURL + endpoint
-const endpoint = 'matcher.lyrics.get';
+const apikey = process.env.LYRICS_API_KEY; // defined in .env
+axios.default.baseURL = 'https://api.musixmatch.com/ws/1.1'; // potentially just have one url variable which is baseURL + endpoint
 
 const lyricsapiController = {};
 
-lyricsapiController.getLyrics = async (req, res, next) => {
-  try {
-    const { songname, artist, trackId } = req.body;
-    const params = {
-      q_track: songname,
-      q_artist: artist,
-      apikey: apiKey,
-    };
-
-    const response = await axios.get(baseUrl + endpoint, { params });
-
-    if (response.status === 200) {
-      const lyrics = response.data.message.body.lyrics;
-
-      if (lyrics) {
-        const lyricsBody = lyrics.lyrics_body;
-        //console.log(lyricsBody)
-        // console.log('Lyrics:\n', lyricsBody);
-        res.locals.lyrics = lyricsBody;
-        res.locals.artist = artist;
-        res.locals.songname = songname;
-        res.locals.trackId = trackId;
-        return next();
-      } else {
-        console.log('No lyrics found');
-        throw new Error('No lyrics found'); // return specific error in object format
+lyricsapiController.getSongs = (req, res, next) => {
+  const { page } = req.params
+  const params = {
+    f_track_release_group_first_release_date_min: 19491212,
+    f_track_release_group_first_release_date_max: 20131212,
+    f_lyrics_language: 'en',
+    s_track_rating: 'desc',
+    page_size: 15,
+    page,
+    apikey,
+  };
+  axios('/track.search', {params})
+  .then(function(data) {
+    const { track_list } = data.message.body;
+    const clean_track_list = track_list.filter(el => el.track.explicit === 0)
+    res.locals.songs = clean_track_list.map(el => {
+      return {
+        track_id: el.trackId,
+        name: el.track_name,
+        artistName: el.artist_name
       }
-    } else {
-      console.log('An error occurred:', response.status);
-      throw new Error('API request failed'); // return specific error in object format
-    }
-  } catch (error) {
-    console.error('An error occurred:', error.message);
-    return next(error);
-  }
+    })
+    return next()
+  })
+  .catch((error) => {
+    next({
+      log: 'Express error handler caught lyricsapiController.getSongs middleware error',
+      status: 400,
+      message: { err: error.message },
+    });
+  })
+}
+
+lyricsapiController.getLyrics = (req, res, next) => {
+  const { id } = req.params;
+  const params = {
+    track_id: id,
+    apikey
+  };
+
+axios('/track.lyrics.get', {params})
+  .then(function(data) {
+    const rawLyrics = data.message.body.lyrics.lyrics_body.split('/n');
+    const lyrics = rawLyrics.slice(0, rawLyrics.indexOf('...'));
+    const jumbledLyrics = lyrics.map(el => {
+      const arr = el.split(' ')
+      for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+      return arr.join(' ');
+    })
+    res.locals.lyrics = jumbledLyrics.join('/n')
+    next()
+  })
+  .catch((error) => {
+    next({
+      log: 'Express error handler caught lyricsapiController.getLyrics middleware error',
+      status: 400,
+      message: { err: error.message },
+    });
+  })
 };
 
 module.exports = lyricsapiController;
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -4,18 +4,16 @@ const gptapiController = require('../controllers/gptapiController');
 const lyricsapiController = require('../controllers/lyricsapiController');
 const databaseController = require('../controllers/databaseController');
 
-router.post(
-  '/lyricsapi',
-  lyricsapiController.getLyrics,
-  gptapiController.genLyrics,
-  databaseController.createEntry,
+router.get(
+  '/getsongs/:page',
+  lyricsapiController.getSongs,
   (req, res) => {
     console.log('Lyrics:', res.locals.lyrics);
-    return res.status(200).json({ lyrics: res.locals.newSong });
+    return res.status(200).json(res.locals.songs);
   }
 );
 
-router.get('/:id', databaseController.getSong, (req, res) => {
-  return res.status(200).json(res.locals.foundSong);
+router.get('/getlyrics/:id', databaseController.getLyrics, (req, res) => {
+  return res.status(200).json(res.locals.lyrics);
 });
 module.exports = router;
